e7e9bcf97b47a0fd12a2d784c6db8e85a4523900
arcngames <arcnarcn77@gmail.com>
2026-03-29 18:21:20 +0300

    Build: Corrected app name to 'Zeka' with appId 'com.zekagame.app'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.zekagame.app',
-  appName: 'ZekaGame',
+  appName: 'Zeka',
   webDir: 'dist',
   plugins: {
     SplashScreen: {
